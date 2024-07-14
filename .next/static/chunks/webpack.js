730888bad94ed9985081f4c42377bb35d6e72f80
Ecoomber327 <78524305+Ecoomber327@users.noreply.github.com>
2024-07-14 13:37:18 -0400

    things are working

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ef7134f79b21e49"; }
+/******/ 		__webpack_require__.h = function() { return "c32b6b6d77a69127"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
